f021fb9d56b8a565399ed79f875b08c3de8e7c81
404-brain <tyknott94@gmail.com>
2018-02-11 04:51:54 -0500

    Added news & exchanges url

--- coins/urls.py
+++ coins/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path(r'', views.index, name='index'),
     path(r'coin-info', views.coin_info, name='coin_rank'),
     path(r'coin-price-info', views.coin_price_info, name='coin_info'),
+    path(r'news', views.crypto_news, name="news"),
+    path(r'exchanges', views.exchanges, name='exchanges'),
 ]
