5b961c5fb032dc9f2c72410707e995b9d84c848d
404-brain <tyknott94@gmail.com>
2018-02-22 03:13:51 -0500

    Update urls.py
    Cleanup

--- coins/urls.py
+++ coins/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path(r'', views.index, name='index'),
-    path(r'coin-info', views.coin_info, name='coin_rank'),
-    path(r'coin-price-info', views.coin_price_info, name='coin_info'),
+    path(r'coin/<str:coin>', views.coin_info, name='coin_info'),
+    path(r'coin-rank-info', views.coin_rank_info, name='coin_rank'),
+    path(r'coin-data', views.coin_data, name='coin_data'),
     path(r'news', views.crypto_news, name="news"),
     path(r'exchanges', views.exchanges, name='exchanges'),
 ]
