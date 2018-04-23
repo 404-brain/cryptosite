a26d05c3b382aacf828a9d005e05f744295e6adf
404-brain <tyknott94@gmail.com>
2018-04-23 02:16:54 -0400

    Search by symbol or name

--- static/coins/js/search.js
+++ static/coins/js/search.js
@@ -1,5 +1,14 @@
 let search_input = document.getElementById('search-input');
-search_input.addEventListener('keyup', search);
+let paginaton = document.querySelector(".paginaton");
+search_input.addEventListener('input', search);
+
+// tables
+const t1 = document.getElementById("table1");
+const t2 = document.getElementById("table2");
+const t3 = document.getElementById("table3");
+const t4 = document.getElementById("table4");
+const t5 = document.getElementById("table5");
+
 
 function search(){
     // declare variables
@@ -9,8 +18,14 @@ function search(){
     // loop through coin_rows
     for(let i = 0; i < coin_row.length; i++){
         let coin_name = coin_row[i].getElementsByTagName('span')[0];
-        if(coin_name){
+        let coin_sym = coin_row[i].getElementsByTagName('span')[1];
+        if(coin_name || coin_sym){
+            // search by coin name
             if(coin_name.innerHTML.toUpperCase().indexOf(input_txt) > -1){
+                coin_row[i].style.display = "";
+                table2.id = "table1";
+            // search by coin symbol
+            }else if(coin_sym.innerHTML.toUpperCase().indexOf(input_txt) > -1){
                 coin_row[i].style.display = "";
             }else{
                 coin_row[i].style.display = "none";
